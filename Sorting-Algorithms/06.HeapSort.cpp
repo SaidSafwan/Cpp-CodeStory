faa66599badc2d037eaaf0d5d1ec2bb392026281
SaidSafwan <saidsafwan59@gmail.com>
2024-11-10 19:11:07 +0530

    Update code removing std from swap

--- Sorting-Algorithms/06.HeapSort.cpp
+++ Sorting-Algorithms/06.HeapSort.cpp
@@ -26,7 +26,7 @@ void heapSort(vector<int>& arr) {
 
     while (n > 0) {
         n = n - 1;
-        std::swap(arr[0], arr[n]);
+        swap(arr[0], arr[n]);
         int parentindex = 0;
         int leftchildidx = (2 * parentindex) + 1;
         int Rightchildidx = (2 * parentindex) + 2;
